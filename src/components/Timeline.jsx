014735ba712d336e6f6dc653d5061ae3a5717746
seetal panda <seetalpanda0903@gmail.com>
2025-07-02 21:05:29 +0530

    first commit

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -35,17 +35,19 @@ export const Timeline = ({ data }) => {
               <div className="absolute flex items-center justify-center w-10 h-10 rounded-full -left-[15px] bg-midnight">
                 <div className="w-4 h-4 p-2 border rounded-full bg-neutral-800 border-neutral-700" />
               </div>
-              <div className="flex-col hidden gap-2 text-xl font-bold md:flex md:pl-20 md:text-4xl text-neutral-300">
-                <h3>{item.date}</h3>
-                <h3 className="text-3xl text-neutral-400">{item.title}</h3>
-                <h3 className="text-3xl text-neutral-500">{item.Company}</h3>
+              <div className="flex-col hidden gap-2 text-xl font-bold md:flex md:pl-20 md:text-4xl text-sand">
+                <h3>{item.title}</h3>
+                <h3 className="text-3xl text-neutral-300">{item.Company}</h3>
+                <h3 className="text-3xl text-neutral-400">{item.date}</h3>
               </div>
             </div>
 
             <div className="relative w-full pl-20 pr-4 md:pl-4">
-              <div className="block mb-4 text-2xl font-bold text-left text-neutral-300 md:hidden ">
-                <h3>{item.date}</h3>
-                <h3>{item.job}</h3>
+              <div className="block mb-4 text-2xl font-bold text-left  md:hidden ">
+                <h3 className="text-xl font-bold text-sand">{item.title}</h3>
+                <h3 className="text-lg font-semibold text-neutral-300">{item.Company}</h3>
+                <h3 className="text-base text-neutral-400">{item.date}</h3>
+                
               </div>
               {item.contents.map((content, index) => (
                 <p className="mb-3 font-normal text-neutral-400" key={index}>
